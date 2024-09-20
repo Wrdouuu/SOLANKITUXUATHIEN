ca1c2212393808bcf47763d8f8b8fbe70da912c9
Wrdouuu <164611150+Wrdouuu@users.noreply.github.com>
2024-09-20 19:59:16 +0700

    s

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+    namespace SOLANKYTUXUATHIEN
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+               Console.Write("Nhap vao chuoi: ");
+            string message = Console.ReadLine();
+            //Xóa khoảng trắng khỏi chuỗi
+            message = message.Replace(" ", string.Empty);
+            //sử dụng vòng lặp while và for để lặp và đếm số lần xuất hiện của ký tự
+            while (message.Length > 0)
+            {
+                Console.Write(message[0] + " : ");
+                int count = 0;
+                for (int j = 0; j < message.Length; j++)
+                {
+                    if (message[0] == message[j])
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine(count);
+                message = message.Replace(message[0].ToString(), string.Empty);
+            }
+            }
+        }
+    }
